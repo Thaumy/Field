ee85547ed21cc930dd0dc22694db7f316ee4bc0e
Thaumy Cheng <Studio_ai@outlook.com>
2020-12-20 00:09:14 +0800

    ~~

---
+++ FieldV/src/components/ListBtn.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app id="main"></v-app>
+</template>
+
+<script>
+export default {
+  name: "ListBtn",
+
+  components: {  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  font-size: 26px;
+  line-height: 50px;
+
+  color: #ffffff;
+}
+
+.main::before {
+  font-family: "field-icon";
+
+  margin: 12px;
+  content: "\e3018";
+}
+
+/* 屏幕宽度 [ 1001 , + ) */
+@media (min-width: 1001px) {
+}
+
+/* 屏幕宽度 ( - , 1000 ] */
+@media (max-width: 1000px) {
+}
+</style>
