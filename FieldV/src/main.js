01f98eef47e430e16909c77d7d1a4c57b6f08016
Thaumy Cheng <Studio_ai@outlook.com>
2020-12-20 00:09:14 +0800

    ~~

--- FieldV/src/main.js
+++ FieldV/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
-import './plugins/axios'
-import App from './App.vue'
+import axios from 'axios'
+import NaviLine from './NaviLine.vue'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
+  axios,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(NaviLine)
+}).$mount('#NaviLine')
