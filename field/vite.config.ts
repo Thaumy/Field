638b51701192626368075313c3b57c15bf0901b4
Thaumy <thaumy@outlook.com>
2022-08-21 13:27:23 +0800

    refactor code highlight

--- field/vite.config.ts
+++ field/vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import {prismjsPlugin} from "vite-plugin-prismjs"
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
@@ -14,6 +15,22 @@ export default defineConfig({
     },
     plugins: [
         vue(),
+        prismjsPlugin({
+            languages: [
+                'c',
+                'go', 'go-module',
+                'sql', 'coq', 'css', 'php', 'cpp',
+                'java', 'html', 'toml', 'json', 'json5', 'yaml', 'scss', 'sass', 'rust',
+                'shell', 'regex', 'latex', 'scala', 'cmake',
+                'python', 'fsharp', 'scheme', 'kotlin',
+                'haskell',
+                'javascript', 'typescript',
+                //rare usage
+                'idris', 'less', 'lisp', 'agda', 'racket', 'julia', 'graphql', 'http', 'lua'
+            ],
+            /*TODO fix style
+            plugins: ['show-language']*/
+        }),
         vuetify({autoImport: true}),
     ],
 })
