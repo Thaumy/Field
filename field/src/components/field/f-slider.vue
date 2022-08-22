e89f992f4692e4f8463d713546659c3fcbf6d71d
Thaumy <thaumy@outlook.com>
2022-08-22 21:09:47 +0800

    add f-slider

---
+++ field/src/components/field/f-slider.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+
+    <div class="f-slider"
+    >
+      <div
+          class="holder"
+          :style="style"
+          v-if="render"
+      >
+        <slot/>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const props = defineProps({
+  afterClosed: {
+    type: Function,
+    default: () => {
+    }
+  }
+})
+
+const render = ref(false)
+const style = ref<{ top: string, height: string }>(null)
+const latestHeight = ref(0)
+
+defineExpose({
+  expand(height: number) {
+    latestHeight.value = height
+    style.value = {
+      top: '-' + height + 'px',
+      height: '0',
+    }
+    render.value = true
+    requestAnimationFrame(() => {
+      style.value = {
+        top: '0',
+        height: height + 'px',
+      }
+    })
+  },
+  close() {
+    style.value = {
+      top: '-' + latestHeight.value + 'px',
+      height: '0',
+    }
+    setTimeout(() => {
+      render.value = false
+      props.afterClosed()
+    }, 200)
+  }
+})
+
+</script>
+
+<style scoped>
+.f-slider {
+  overflow: hidden;
+}
+
+.holder {
+  position: relative;
+  transition: all 0.2s ease
+}
+</style>
