b8d0d84f6baf89b980b7bdd1b7bca597faef26e4
Thaumy <thaumy@outlook.com>
2022-09-01 05:57:17 +0800

    Squashed commit of the following:
    commit 7e6a6c72f729bfa1853b5c1a2ed78c3c9ca52e3e
    Author: Thaumy <thaumy@outlook.com>
    Date:   Thu Sep 1 05:56:42 2022 +0800
    
        beautify
    
    commit d47e63ab3e11a720a6b2338763d7fbe779986ad6
    Author: Thaumy <thaumy@outlook.com>
    Date:   Wed Aug 31 09:24:21 2022 +0800
    
        Update GoUpBtn.vue
    
    commit 5273fb7b8f9ab5888d6535951639b5fcbe9e7fd7
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Aug 30 23:23:28 2022 +0800
    
        adjust light theme
    
    commit a0339322e2e43c135e88732c9706b8b8c05cbc15
    Author: Thaumy <thaumy@outlook.com>
    Date:   Mon Aug 29 19:09:11 2022 +0800
    
        refactor
    
    commit 890fe5dbca371da9e77b7a9f69e708949a908bdc
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sun Aug 28 23:06:44 2022 +0800
    
        add themes
    
    commit 106d62d33d14ce7b093c17c2f070873d619f83da
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sun Aug 28 19:31:45 2022 +0800
    
        fix flex
    
    commit bd6ba2cca28ecbccd89b9c845aa66276dfd5b064
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sun Aug 28 16:07:30 2022 +0800
    
        add auto position of snackbar
    
    commit b1601ae5dfcdb2027ce4bacad27f1246c1e8fb4b
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sun Aug 28 13:30:09 2022 +0800
    
        refactor
    
    commit 760b64f9dc82c64db561ce7b9d9f41650c493a10
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sun Aug 28 12:09:58 2022 +0800
    
        refactor
    
    commit cbd8c17fbdee74d25d1608038fe870a0d964650a
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Aug 26 19:56:22 2022 +0800
    
        add read time calculation
    
    commit f8d5ff7f1a20895d96ae12dbefa89d54386a7073
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Aug 26 12:55:11 2022 +0800
    
        fix event listener
    
    commit 0c4acfb81ba42c40c8ab004ce048ef374534f465
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Aug 26 11:11:34 2022 +0800
    
        add tab disable function
    
    commit 7078f39eee35e672f4fb617beb6222014e63e85a
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Aug 26 06:47:03 2022 +0800
    
        add mkd render and f-divider
    
    commit 9a97458306bc9d099bf167766933124cb55bbf30
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Aug 26 00:28:30 2022 +0800
    
        add post navi
    
    commit 8784efff79ccce54ef99b4cb42dfa28efb884fb2
    Author: Thaumy <thaumy@outlook.com>
    Date:   Thu Aug 25 21:45:21 2022 +0800
    
        refactor navi col
    
    commit 1e5a65f7a28bb80cc87fcbd0eede9acdca6eb740
    Author: Thaumy <thaumy@outlook.com>
    Date:   Thu Aug 25 07:28:46 2022 +0800
    
        refactor
    
    commit 78d8e9509b2d564e89adbd1ab079c15bcda57695
    Author: Thaumy <thaumy@outlook.com>
    Date:   Wed Aug 24 12:55:54 2022 +0800
    
        refactor go-up-btn, add f-card
    
    commit 5220cb312fd6c79538fe9649c0f79a37c4e15bce
    Author: Thaumy <thaumy@outlook.com>
    Date:   Tue Aug 23 23:15:48 2022 +0800
    
        refactor comment editor
    
    commit 447e56df4bffb1d7758a07de4780a025432ee754
    Author: Thaumy <thaumy@outlook.com>
    Date:   Mon Aug 22 22:10:19 2022 +0800
    
        enable stylus

--- field/src/scripts/theme/fieldDark.ts
+++ field/src/scripts/theme/fieldDark.ts
@@ -2,7 +2,10 @@ import {ThemeDefinition} from "vuetify/dist/vuetify";
 
 export const fieldDark: ThemeDefinition = {
     dark: true,
-    variables: {},
+    variables: {
+        'medium-emphasis-opacity': 0.6,
+        'high-emphasis-opacity': 0.9,
+    },
     colors: {
         background: "#000000",
         'on-background': "#F0F0F0",//240
@@ -10,21 +13,20 @@ export const fieldDark: ThemeDefinition = {
         surface: "#1E1E1E",//30
         'on-surface': "#F0F0F0",//240
 
-        primary: "#000000",
+        primary: "#0199ff",
         'on-primary': "#000000",
 
-        secondary: "#000000",
-        'on-secondary': "#000000",
+        secondary: "#238636",
+        'on-secondary': "#F0F0F0",
+
+
 
         success: "#000000",
         'on-success': "#000000",
-
         warning: "#000000",
         'on-warning': "#000000",
-
         error: "#000000",
         'on-error': "#000000",
-
         info: "#000000",
         'on-info': "#000000",
     },
