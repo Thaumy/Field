6183577bed346ad37b8b8e6f9290da78b5326d0b
Thaumy <thaumy@outlook.com>
2022-09-03 08:35:15 +0800

    fix git conflict

--- field/src/components/btn/SwitchZone.vue
+++ field/src/components/btn/SwitchZone.vue
@@ -1,37 +1,15 @@
 <template>
   <div>
 
-<<<<<<< Updated upstream
-    <f-card
-        :style="{margin:'auto'}"
-    >
-      <div class="switch-zone">
-        <v-chip
-            class="prev-btn cursor-pointer"
-            variant="text"
-            v-if="prev"
-=======
     <div class="holder border-radius-all">
       <div class="prev-chip cursor-pointer">
         <v-chip
             prepend-icon="mdi-chevron-left"
             color="primary"
             variant="text"
->>>>>>> Stashed changes
         >
-          <v-icon icon="mdi-chevron-left"/>
           {{ prev }}
         </v-chip>
-<<<<<<< Updated upstream
-
-        <v-chip
-            class="next-btn cursor-pointer"
-            variant="text"
-            v-if="next"
-        >
-          {{ next }}
-          <v-icon icon="mdi-chevron-right"/>
-=======
       </div>
 
       <div class="next-chip cursor-pointer">
@@ -41,18 +19,15 @@
             variant="text"
         >
           {{ next }}
->>>>>>> Stashed changes
         </v-chip>
       </div>
-    </f-card>
+    </div>
 
   </div>
 </template>
 
 <script lang="ts" setup>
-import FCard from "@/components/field/f-card.vue";
 import {defineProps} from "vue";
-import FCard from "@/components/field/f-card.vue";
 
 defineProps({
   prev: {type: String, default: null},
@@ -61,12 +36,8 @@ defineProps({
 </script>
 
 <style lang="stylus" scoped>
-<<<<<<< Updated upstream
-.switch-zone
-=======
 .holder
   backdrop-filter blur(20px)
->>>>>>> Stashed changes
   display flex
   justify-content end
   flex-flow wrap
@@ -74,19 +45,9 @@ defineProps({
 .prev-chip
   margin-right auto
 
-<<<<<<< Updated upstream
-.next-btn
-  margin-left 1rem
-
-.prev-btn
-.next-btn
-  color rgba(1 153 255 1)
-
-=======
 @css {
   .holder {
     background-color: rgba(var(--v-theme-background), 0.7)
   }
 }
->>>>>>> Stashed changes
 </style>
