d11e45a985bb3d08ffb94919737706fe271b0711
Thaumy <thaumy@outlook.com>
2022-09-22 18:22:48 +0800

    refactor to async syntax sugar

--- field/src/scripts/data/ws.ts
+++ field/src/scripts/data/ws.ts
@@ -1,71 +1,74 @@
-export function wsRequestAllPostId() {
+export {
+    wsRequestAllPostId,
+    wsRequestPost,
+    wsRequestPrevPost,
+    wsRequestNextPost
+}
+
+async function wsRequestAllPostId() {
     const ws = new WebSocket("ws://localhost:8080/get_all_post_id")
 
     //TODO event remove
     ws.addEventListener('open', () => ws.send(''))
 
-    return new Promise<string>
-    ((resolve) => {
-            ws.addEventListener("message", ev => {
-                resolve(ev.data)
-            })
-        }
-    ).then(responseJson => {
-        ws.close()
-        return responseJson
-    })
+    const task = new Promise<string>
+    (resolve =>
+        ws.addEventListener("message", ev => resolve(ev.data))
+    )
+
+    const responseJson = await task
+    ws.close()
+
+    return responseJson
 }
 
-export function wsRequestPost(id: number) {
+async function wsRequestPost(id: number) {
     const ws = new WebSocket("ws://localhost:8080/get_post")
 
     //TODO event remove
     ws.addEventListener('open', () => ws.send(id.toString()))
 
-    return new Promise<string>
-    ((resolve) => {
-            ws.addEventListener("message", ev => {
-                resolve(ev.data)
-            })
-        }
-    ).then(responseJson => {
-        ws.close()
-        return responseJson
-    })
+    const task = new Promise<string>
+    (resolve =>
+        ws.addEventListener("message", ev => resolve(ev.data))
+    )
+
+    const responseJson = await task
+    ws.close()
+
+    return responseJson
 }
 
-export function wsRequestPrevPost(current_post_id: number) {
+async function wsRequestPrevPost(current_post_id: number) {
     const ws = new WebSocket("ws://localhost:8080/get_prev_post")
 
     //TODO event remove
     ws.addEventListener('open', () => ws.send(current_post_id.toString()))
 
-    return new Promise<string>
-    ((resolve) => {
-            ws.addEventListener("message", ev => {
-                resolve(ev.data)
-            })
-        }
-    ).then(responseJson => {
-        ws.close()
-        return responseJson
-    })
+    const task = new Promise<string>
+    (resolve =>
+        ws.addEventListener("message", ev => resolve(ev.data))
+    )
+
+    const responseJson = await task
+    ws.close()
+
+    return responseJson
 }
 
-export function wsRequestNextPost(current_post_id: number) {
+async function wsRequestNextPost(current_post_id: number) {
     const ws = new WebSocket("ws://localhost:8080/get_next_post")
 
     //TODO event remove
     ws.addEventListener('open', () => ws.send(current_post_id.toString()))
 
-    return new Promise<string>
-    ((resolve) => {
-            ws.addEventListener("message", ev => {
-                resolve(ev.data)
-            })
-        }
-    ).then(responseJson => {
-        ws.close()
-        return responseJson
-    })
+    const task = new Promise<string>
+    (resolve =>
+        ws.addEventListener("message", ev => resolve(ev.data))
+    )
+
+    const responseJson = await task
+    ws.close()
+
+    return responseJson
 }
