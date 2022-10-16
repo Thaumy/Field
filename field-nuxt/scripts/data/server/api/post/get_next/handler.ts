4715616279cc434e8bd360ffe111d18078db5fd4
Thaumy <thaumy@outlook.com>
2022-10-16 16:14:23 +0800

    add server side ws conn close

--- field-nuxt/scripts/data/server/api/post/get_next/handler.ts
+++ field-nuxt/scripts/data/server/api/post/get_next/handler.ts
@@ -4,9 +4,10 @@ import WebSocketSSR from "ws"
 import {request, wsRoot} from "~/scripts/data/server/helper"
 
 const api_path = '/post/get_next'
-const conn = new WebSocketSSR(`${wsRoot}${api_path}`)
 
 export async function handler(req: Req) {
+    const conn = new WebSocketSSR(`${wsRoot}${api_path}`)
     const api_rsp = request<Req, Rsp>(api_path, conn, req)
+    conn.close()
     return await api_rsp
 }
