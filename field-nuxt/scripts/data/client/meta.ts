c0184b6999191c59440de8b7e0d966ce66784db7
Thaumy <thaumy@outlook.com>
2022-10-18 07:23:37 +0800

    code refactor

---
+++ field-nuxt/scripts/data/client/meta.ts
@@ -0,0 +1,3 @@
+export const wsRoot = 'wss://next.thaumy.cn:8081'
+//export const wsRoot = 'ws://127.0.0.1:8081'
+
--- field/src/scripts/util/nullable.ts
+++
@@ -1,9 +0,0 @@
-function nullOrUndefined(x: any): x is null | undefined {
-    return x === null || x === undefined
-}
-
-function notNullOrUndefined(x: any) {
-    return !nullOrUndefined(x)
-}
-
-export {nullOrUndefined, notNullOrUndefined}
