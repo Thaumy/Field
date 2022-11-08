751c7fdc65f312a43d6b9fd8263c589ee821d5cf
Thaumy <thaumy@outlook.com>
2022-11-08 10:28:50 +0800

    refactor

--- field-nuxt/ws/client/api/comment/create/handler.ts
+++ field-nuxt/ws/client/api/comment/create/handler.ts
@@ -1,15 +1,11 @@
 import {Req} from "@/ws/client/api/comment/create/req"
 import {Rsp} from "@/ws/client/api/comment/create/rsp"
-import {request} from "@/ws/client/helper"
+import {makeHandler, request} from "@/ws/client/helper"
 import {wsClientRoot} from "@/ws/meta"
 
 const api_path = '/comment/create'
 let conn = new WebSocket(`${wsClientRoot}${api_path}`)
 
 export async function handler(req: Req) {
-    if (conn.CLOSED) {
-        conn = new WebSocket(`${wsClientRoot}${api_path}`)
-    }
-    const api_rsp = request<Req, Rsp>(api_path, conn, req)
-    return await api_rsp
+    return makeHandler<Req, Rsp>(wsClientRoot, api_path, req, conn)
 }
--- field-nuxt/ws/server/api/post/get_menu/handler.ts
+++ field-nuxt/ws/server/api/post/get_menu/handler.ts
@@ -1,14 +1,11 @@
 import {Req} from "@/ws/server/api/post/get_menu/req"
 import {Rsp} from "@/ws/server/api/post/get_menu/rsp"
 import WebSocketSSR from "ws"
-import {request} from "@/ws/server/helper"
+import {makeHandler, request} from "@/ws/server/helper"
 import {wsServerRoot} from "@/ws/meta"
 
 const api_path = '/post/get_menu'
 
 export async function handler(req: Req) {
-    const conn = new WebSocketSSR(`${wsServerRoot}${api_path}`)
-    const api_rsp = await request<Req, Rsp>(api_path, conn, req)
-    conn.close()
-    return api_rsp
+    return makeHandler<Req, Rsp>(wsServerRoot, api_path, req)
 }
